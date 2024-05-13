1e492903bd3507fb0d33e76fd30aaa6f47bcc53a
riaj87 <58988945+riaj87@users.noreply.github.com>
2024-05-13 11:35:36 +0000

    Read from db

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+import sqlalchemy
 
 app = Flask(__name__)
 
-JOBS = [{
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 90,000'
-}, {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-}, {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-}, {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
-}]
+# JOBS = [{
+#     'id': 1,
+#     'title': 'Data Analyst',
+#     'location': 'Bengaluru, India',
+#     'salary': 'Rs. 90,000'
+# }, {
+#     'id': 2,
+#     'title': 'Data Scientist',
+#     'location': 'Delhi, India',
+#     'salary': 'Rs. 15,00,000'
+# }, {
+#     'id': 3,
+#     'title': 'Frontend Engineer',
+#     'location': 'Remote',
+# }, {
+#     'id': 4,
+#     'title': 'Backend Engineer',
+#     'location': 'San Francisco, USA',
+#     'salary': '$150,000'
+# }]
+
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(sqlalchemy.text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+        return jobs
 
 
 @app.route('/')
 def test():
-    return render_template('home.html', jobs=JOBS, company_name='Jovian')
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, company_name='Jovian')
 
 
 @app.route("/api/jobs")
---
+++ database.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine(
+    "mysql+pymysql://sql6704968:m2kqyVzntf@sql6.freesqldatabase.com/sql6704968?charset=utf8mb4"
+)
