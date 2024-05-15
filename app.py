7059143fdb974c69e23f75a66c20c40538fff1c6
riaj87 <58988945+riaj87@users.noreply.github.com>
2024-05-15 08:38:21 +0000

    Dynamic Page

--- app.py
+++ app.py
@@ -1,40 +1,27 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
-# JOBS = [{
-#     'id': 1,
-#     'title': 'Data Analyst',
-#     'location': 'Bengaluru, India',
-#     'salary': 'Rs. 90,000'
-# }, {
-#     'id': 2,
-#     'title': 'Data Scientist',
-#     'location': 'Delhi, India',
-#     'salary': 'Rs. 15,00,000'
-# }, {
-#     'id': 3,
-#     'title': 'Frontend Engineer',
-#     'location': 'Remote',
-# }, {
-#     'id': 4,
-#     'title': 'Backend Engineer',
-#     'location': 'San Francisco, USA',
-#     'salary': '$150,000'
-# }]
 
 
 @app.route('/')
 def test():
     jobs = load_jobs_from_db()
-    return render_template('home.html', jobs=jobs, company_name='Jovian')
+    return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 import os
 
 my_secret = os.environ['DB_CONNECTION_STRING']
@@ -14,3 +14,15 @@ def load_jobs_from_db():
         for row in result.all():
             jobs.append(row._asdict())
         return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            sqlalchemy.text("select * from jobs where id = :val"), {"val": id})
+        rows = result.all()
+
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
